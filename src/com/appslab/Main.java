258f66c58b99283a7cb04c8fecd786daa257140e
MartinHolena <martinkiko18@gmail.com>
2021-09-15 10:06:26 +0200

    Branch palindrom

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,21 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        System.out.println(isPalindrome("patap"));
+        System.out.println(isPalindrome("tanap"));
     }
+
+    static boolean isPalindrome(String str)
+    {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+}
 }
