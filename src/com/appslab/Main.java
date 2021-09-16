797aaf98b47404265461be67bc6e946dc28e42cc
martinorion <martinhox03@gmail.com>
2021-09-16 08:19:29 +0200

    branch maxAndMin

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,26 @@
 package com.appslab;
 
+import javafx.print.Collation;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        System.out.println(differenceMaxMin(new int[]{-2, 1, 2, 3, 4, 5, 6}));
     }
+   static int differenceMaxMin(int[] array){
+
+        int min = Arrays.stream(array)
+               .min()
+               .getAsInt();
+
+        int max = Arrays.stream(array)
+               .max()
+               .getAsInt();
+
+        return min > max? min - max : max - min;
+
+   }
 }
