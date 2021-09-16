32677495cacb7636258667f72a40cb51ac6014b8
martinorion <martinhox03@gmail.com>
2021-09-16 08:56:56 +0200

    branch checkEnding

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,26 +1,16 @@
 package com.appslab;
 
-import javafx.print.Collation;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(warOfNumbers(new int[]{1, 2, 3, 4, 5, 6}));
+        System.out.println(checkEnding("matko", "tko"));
+        System.out.println(checkEnding("matko", "sko"));
     }
-   static int warOfNumbers(int[] array){
-    int a = 0;
-    int b = 0;
-        for (int i = 0; i < array.length; i++){
-            if(array[i] % 2 == 0){
-            a += array[i];
-            }
-            else{
-             b += array[i];
-            }
-        }
-        return a < b? b - a : a - b;
+
+     static boolean checkEnding(String m, String n){
+
+        return m.endsWith(n);
    }
 }
+
